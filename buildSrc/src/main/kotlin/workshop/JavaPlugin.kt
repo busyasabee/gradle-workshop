cc577f81d2641b0351fb2d50e85e1cb3a5ee1ad7
rider <dmitr.romashov@gmail.com>
2024-07-06 17:30:18 +0300

    Add jar task

--- buildSrc/src/main/kotlin/workshop/JavaPlugin.kt
+++ buildSrc/src/main/kotlin/workshop/JavaPlugin.kt
@@ -2,15 +2,24 @@ package workshop
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.internal.util.Trie.from
+import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.register
 
 class JavaPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         apply(plugin = "base")
-        tasks.register("compile", CompileTask::class.java) {
+        val compileTask = tasks.register("compile", CompileTask::class.java) {
             sourceDir.set(layout.projectDirectory.dir("src/main/java"))
             classDir.set(layout.buildDirectory.dir("$name/classes"))
+        }
+
+        tasks.register<Jar>("jar") {
+            from(compileTask/*.flatMap { it.classDir }*/)
+            destinationDirectory.set(layout.buildDirectory.dir("libs"))
+//            into(layout.buildDirectory.dir("libs"))
         }
     }
 }
